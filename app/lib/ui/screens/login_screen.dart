b935178a0580d795acd7b35d2e46e11831d7e1b3
XyCloudStore Dev <dev@xycloud.my.id>
2026-09-06 21:11:03 +0000

    Rapikan sisa merge: hapus method register lama

--- app/lib/ui/screens/login_screen.dart
+++ app/lib/ui/screens/login_screen.dart
@@ -108,14 +108,6 @@ class _LoginScreenState extends State<LoginScreen> {
     });
   }
 
-  void _gantiMode() {
-    setState(() {
-      daftar = !daftar;
-      _galat = null;
-      _form.currentState?.reset();
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     final loading = context.watch<AppState>().loading;
